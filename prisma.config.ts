660d4b5636f044db61f013c2f96eee9bf7bd7786
FHC <38954889+FHCProm@users.noreply.github.com>
2026-06-13 02:33:05 +0000

    removed unused prisma generated folder

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,5 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "./src/generated/prisma/client"; // Adjust path if needed
+import { PrismaClient } from "./generated/prisma/client"; // Adjust path if needed
 
 const connectionString = process.env.DATABASE_URL ?? "";
 
